d77fc02632a28d4058ac92e6f1f1f63c7cc76ce5
kavi-786 <kavimari879@gmail.com>
2026-05-04 09:59:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -96,4 +96,4 @@ def page_not_found(e):
 # =========================
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 10000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=port, debug=True)
