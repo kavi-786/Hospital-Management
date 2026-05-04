c3ca264ede3b616009663fcb1380e6b823d777dc
kavi-786 <kavimari879@gmail.com>
2026-05-04 12:09:15 +0530

    Update main_routes.py

--- routes/main_routes.py
+++ routes/main_routes.py
@@ -5,13 +5,14 @@ main_bp = Blueprint('main', __name__)
 @main_bp.route('/')
 def index():
     cursor = g.db.cursor()
-    # Fetch some doctors to show on the home page
+
     cursor.execute('''
-    SELECT u.name, u.image, d.specialization 
+    SELECT u.name, d.image, d.specialization 
     FROM doctors d
     JOIN users u ON d.user_id = u.id
     WHERE d.availability_status = TRUE
     LIMIT 4
-''')
+    ''')
+
     doctors = cursor.fetchall()
     return render_template('index.html', doctors=doctors)
